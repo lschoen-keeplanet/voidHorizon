f7f38b4be373c4134619dabfdc6ee72aa96ed081
lschoen-keeplanet <82098138+lschoen-keeplanet@users.noreply.github.com>
2024-05-16 20:03:36 +0200

    test

--- voidHorizon.js
+++ voidHorizon.js
@@ -1,7 +1,7 @@
 import VHItemSheet from "./module/sheets/VHItemSheet.js";
 
 Hooks.once("init", function(){
-    console.log("%c JS operationnel ", 'color:lime;');
+    console.log("%c JS custom operationnel ", 'color:lime;');
     
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("voidHorizon", VHItemSheet, {makeDefault :true });
